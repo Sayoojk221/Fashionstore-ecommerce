80a6ff33eb167b64bcf643c0537ad3b9e0293864
sayooj k <sayoojk221@gmail.com>
2020-08-30 20:17:28 +0530

    database migrations problem.so local database changed

--- fashionstore/settings.py
+++ fashionstore/settings.py
@@ -80,17 +80,14 @@ WSGI_APPLICATION = 'fashionstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fashionstore',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
